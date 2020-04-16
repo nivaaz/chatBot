5f2a739edb48516e0f0ff26f97426fe5d621b62b
nivaaz <nksehmbhi@gmail.com>
2020-04-16 17:24:53 +1000

    updated question

--- chatbot/src/components/Question/Question.js
+++ chatbot/src/components/Question/Question.js
@@ -1,7 +1,8 @@
 import React from "react"
 
-
 export const Question = (props) =>{
-    
-    return (<p> {props.question} </p>)
+    if (props.questionIndex <= props.currentQuestion){
+        return <p> {props.question}</p>
+    }
+    return <> </>
 }

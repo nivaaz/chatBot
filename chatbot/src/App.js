435ac5eaa781f8b2f96fd2288cd403b9fd7b1061
nivaaz <nksehmbhi@gmail.com>
2020-04-16 16:08:56 +1000

    init

---
+++ chatbot/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Chat App</h1>
+      </header>
+      <body>
+        <h2> chat ah2p body</h2>
+      </body>
+      <footer>
+        <p> footer of app</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

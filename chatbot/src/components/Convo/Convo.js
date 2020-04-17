d3377770a30f4a131337c1237c124b066d50b6f4
nivaaz <nksehmbhi@gmail.com>
2020-04-17 15:40:08 +1000

    formatting

--- chatbot/src/components/Convo/Convo.js
+++ chatbot/src/components/Convo/Convo.js
@@ -7,51 +7,54 @@ import "./convo.css";
 
 export class Convo extends React.Component {
     state = {
-        currentQuestion: 0, 
+        currentQuestion: 0,
         answers: []
     };
-    onClickResponse = (e)=>{
+    onClickResponse = (e) => {
         const questionIndex = Number(e.target.name);
         const answerResult = Number(e.target.value);
         let ans = this.state.answers;
         ans[questionIndex] = answerResult;
 
         this.setState({
-            currentQuestion: questionIndex+1,
-            answers:ans}, 
+            currentQuestion: questionIndex + 1,
+            answers: ans
+        },
             console.log(
                 "state",
                 this.state.answers,
                 this.state.currentQuestion));
     }
     renderConversation = () => {
-        const conversation = convo.map((key, index)=>{
-            return (<div key = {key}>
-            <Question 
-                question={key.question}
-                questionIndex = {index}
-                currentQuestion = {this.state.currentQuestion}
-            />
-            <Response
-                responses = {key.responses}
-                currentQuestion = {this.state.currentQuestion}
-                answers = {this.state.answers}
-                questionIndex = {index}
-                handleResponseClick = {this.onClickResponse}
-            />
-            <CustomResponse
-                questionIndex = {index}
-                currentQuestion = {this.state.currentQuestion}
-                answers = {this.state.answers}
-                customResponse = {key.customResponse}
-            />
+        const conversation = convo.map((key, index) => {
+            return (<div key={key}>
+                <Question
+                    question={key.question}
+                    questionIndex={index}
+                    currentQuestion={this.state.currentQuestion}
+                />
+                <Response
+                    responses={key.responses}
+                    currentQuestion={this.state.currentQuestion}
+                    answers={this.state.answers}
+                    questionIndex={index}
+                    handleResponseClick={this.onClickResponse}
+                />
+                <CustomResponse
+                    questionIndex={index}
+                    currentQuestion={this.state.currentQuestion}
+                    answers={this.state.answers}
+                    customResponse={key.customResponse}
+                />
             </div>)
         })
-    return (<div className = "convo-container">{conversation}</div>)
+        return (<div className="convo-container">{conversation}</div>)
     }
-    render (){
-        return (<>
-        {this.renderConversation()}
-        </>)
+    render() {
+        return (
+            <>
+                {this.renderConversation()}
+            </>
+        )
     }
 }
--- chatbot/src/components/Question/Question.js
+++ chatbot/src/components/Question/Question.js
@@ -1,8 +1,12 @@
 import React from "react"
 
-export const Question = (props) =>{
-    if (props.questionIndex <= props.currentQuestion){
-        return <p className = "bubble" key={props.questionIndex}> {props.question}</p>
+export const Question = (props) => {
+    if (props.questionIndex <= props.currentQuestion) {
+        return (
+            <p className="bubble" key={props.questionIndex}>
+                {props.question}
+            </p>
+        )
     }
     return <> </>
 }

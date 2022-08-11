53674604a6c31c8068dbef23789ff62d9a56bac8
nivaaz <nksehmbhi@gmail.com>
2022-08-11 16:46:15 +1000

    fix: remove duplicate provider

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Convo } from "./components/Convo/Convo.tsx";
-import { FormStateProvider } from "./components/formStates";
 
 function App() {
   return (
@@ -10,9 +9,7 @@ function App() {
         <h1>Chat App</h1>
       </header>
       <h2> Nivaaz has joined the chat.</h2>
-      <FormStateProvider>
         <Convo />
-      </FormStateProvider>
       <footer>
         <p> Built by Nivaaz Sehmbhi, 2020</p>
       </footer>
